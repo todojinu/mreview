86524684bef9d1e2052b3be566c6604c6727fb42
todojinu <nukukkoya@gmail.com>
2021-11-04 01:14:04 +0900

    2.프로젝트 설정 및 라이브러리 추가

--- src/main/java/org/zerock/mreview/MreviewApplication.java
+++ src/main/java/org/zerock/mreview/MreviewApplication.java
@@ -2,7 +2,9 @@ package org.zerock.mreview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(value = "file:c:/dev/mreview/config/config.properties")  //별도로 분리된 DB설정 파일 로드를 위해 추가
 @SpringBootApplication
 public class MreviewApplication {
 
--- src/test/java/org/zerock/mreview/MreviewApplicationTests.java
+++ src/test/java/org/zerock/mreview/MreviewApplicationTests.java
@@ -1,13 +1,18 @@
 package org.zerock.mreview;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MreviewApplicationTests {
 
+	@Value("${spring.datasource.driver-class-name}")
+	private String driver;
+
 	@Test
 	void contextLoads() {
+		System.out.println(driver);
 	}
 
 }
